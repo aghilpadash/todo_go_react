8b84d7d9d0ef5cf82bfc7f770194b0d5d5fa0dd6
Aghil Padash <aghilpadash73@gmail.com>
2021-10-26 17:16:25 +0330

    complete crud services in front

--- react-todos/src/services/todoService.js
+++ react-todos/src/services/todoService.js
@@ -6,3 +6,36 @@ export const loadTodos = () => {
 export const getTodos = (id) => {
     return fetch(`${baseUrl}/${id}`).then(res => res.json());
 }
+
+export const createTodos = (id) => {
+    return fetch(baseUrl, {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            titles: todo.title,
+            complete: todo.complete
+        })
+    }).then((res) => res.json())
+};
+
+export const updateTodos = (id) => {
+    return fetch(`${baseUrl}/${todo.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            id: todo.id,
+            titles: todo.title,
+            complete: todo.complete
+        })
+    }).then((res) => res.json())
+};
+
+export const deleteTodo = (id) => {
+    return fetch(`${baseUrl}/${id}`, {
+        method: 'DELETE',
+    }).then(res => res.JSON());
+}
