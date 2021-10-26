462fab091c2e8db765aa3d7604fa697f767b66e9
Aghil Padash <aghilpadash73@gmail.com>
2021-10-26 21:04:21 +0330

    build todo items

---
+++ react-todos/src/components/todoForm.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {Form, Row, Col, Button, Input} from 'antd';
+import {PlusCircleFilled} from '@ant-design/icons';
+import Form from 'rc-field-form/es/Form';
+
+const TodoForm = ({onFormSubmit}) => {
+    const [form] = Form.useForm();
+    const onFinish = () => {
+        onFormSubmit({
+            title: form.getFieldValue('title'),
+            complete: false
+        });
+        console.log(form.getFieldValue('title'));
+        form.resetFields();
+    }
+    return (
+        <Form
+            form={form}
+            onFinish={onFinish}
+            layout="horizontal"
+            className="todo-form">
+
+            <Row gutters={20}>
+                <Col xs={24} sm={24} md={17} lg={19} xl={24}>
+                    <Form.Item
+                        name={'title'}
+                        rules={[{required: true, message: 'پر کردن این فیلد الزامیست !'}]}>
+                        <Input placeholder="چه کاری میخوای انجام بدی ؟"/>
+                    </Form.Item>
+                </Col>
+                <Col xs={24} sm={24} md={7} lg={5} xl={4}>
+                    <Button type="primary" htmlType="submit" block>
+                        <PlusCircleFilled/>
+
+                    </Button>
+                </Col>
+
+            </Row>
+        </Form>
+    )
+}
+
+export default TodoForm;
---
+++ react-todos/src/components/todoItem.jsx
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+import {Tooltip, Tag, List, Button, Popconfirm, Switch} from 'antd';
+import {CloseOutlined, CheckOutlined} from '@ant-design/icons'
+
+const Todo = ({todo, onTodoRemoval, onTodoToggle}) => {
+    return (
+        <List.Item
+            actions={[
+                <Tooltip
+                    title={todo.complete ? 'هنوز انجام نشده' : 'انجام شد'}>
+                    <Switch
+                        checkedChildren={<CheckOutlined/>}
+                        unCheckedChildren={<CloseOutlined/>}
+                        onChange={()=> onTodoToggle(todo)}
+                        defaultChecked={todo.completed}
+                    />
+                </Tooltip>,
+                <Popconfirm
+                title={'مطمئنی میخوای پاکش کنی ؟'}
+                onConfirm={()=>{
+                    onTodoRemoval(todo);
+                }}>
+                    <Button className="remove-todo-button" type="primary" danfer>
+                        X
+                    </Button>
+                </Popconfirm>
+
+            ]}
+            className="list-item"
+            key={todo.id}>
+        
+            <div className="todo-item">
+                <Tag color={todo.complete ? 'cyan' : 'red'} className="todo-tag">
+                    {todo.title}
+                </Tag>
+            </div>
+        </List.Item>
+
+    )
+}
+export default Todo;
